1ca072bd3d0223f9268f1594076b8b4117fc293d
BENJYY1337 <pernot.benjamin@outlook.fr>
2021-01-07 12:08:40 +0100

    add InfoInCard and icon

--- src/component/InfoInCard.js
+++ src/component/InfoInCard.js
@@ -7,26 +7,82 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
+import CodeIcon from "@material-ui/icons/Code";
 
 const useStyles = makeStyles({
   root: {},
+
+  colorTitle: {
+    color: "#141C3A",
+  },
+
+  colorSubtitle: {
+    color: "#7510F7",
+  },
 });
 
 export default function InfoInCard(props) {
   const classes = useStyles();
 
   return (
-    <Box my={2} align="center">
+    <Box my={-5} align="center">
       <Card variant="outlined" style={{ maxWidth: "300px" }}>
         <CardContent align="center">
           <Grid container>
             <Grid item xs={12}>
               <Grid item xs>
-                <Typography>Developer Front-end</Typography>
+                <CodeIcon />
               </Grid>
-              <Grid item xs>
-                <Typography>ntm</Typography>
-              </Grid>
+
+              <Box mt={3}>
+                <Grid item xs>
+                  <Typography variant="h5" className={classes.colorTitle}>
+                    <strong>Front-end Developer</strong>
+                  </Typography>
+                </Grid>
+              </Box>
+
+              <Box mt={3}>
+                <Grid item xs>
+                  <Typography>
+                    J'aime coder des choses à partir de zéro et j'aime donner
+                    vie à des idées
+                  </Typography>
+                </Grid>
+              </Box>
+
+              <Box mt={3}>
+                <Grid item xs>
+                  <Typography className={classes.colorSubtitle}>
+                    Languages:
+                  </Typography>
+                </Grid>
+              </Box>
+
+              <Box mt={1}>
+                <Grid item xs>
+                  <Typography>HTML, CSS, Sass, ReactJS, MaterialUI</Typography>
+                </Grid>
+              </Box>
+
+              <Box mt={3}>
+                <Grid item xs>
+                  <Typography className={classes.colorSubtitle}>
+                    Outils dev:
+                  </Typography>
+                </Grid>
+              </Box>
+
+              <Box mt={1}>
+                <Grid item xs>
+                  <Typography>Github</Typography>
+                  <Typography>Figma</Typography>
+                  <Typography>Adobe XD</Typography>
+                  <Typography>Slack</Typography>
+                  <Typography>VSCode</Typography>
+                  <Typography>Terminal</Typography>
+                </Grid>
+              </Box>
             </Grid>
           </Grid>
         </CardContent>
